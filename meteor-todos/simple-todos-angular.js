10ab0ea01f3d980f021d4ce342ee0d4cf20d51ab
Mason Long <masonlong92@gmail.com>
2015-11-18 14:51:35 -0800

    add user authentication for adding new items

--- meteor-todos/simple-todos-angular.js
+++ meteor-todos/simple-todos-angular.js
@@ -1,9 +1,13 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
  
   // This code only runs on the client
-  angular.module('simple-todos',['angular-meteor']);
+ angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
 
   function onReady() {
     angular.bootstrap(document, ['simple-todos']);
@@ -25,7 +29,9 @@ if (Meteor.isClient) {
       $scope.addTask = function (newTask) {
         $scope.tasks.push( {
           text: newTask,
-          createdAt: new Date() }
+          createdAt: new Date(),
+          owner: Meteor.userId(),
+          username: Meteor.user().username }
         );
       };
 
